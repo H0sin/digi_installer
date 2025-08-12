c5bf8aa2c5efdda919b58d6fe1dd14e491c961ed
Hossein <76825182+H0sin@users.noreply.github.com>
2025-08-12 01:41:50 -0700

    add Redis and RedisInsight services to docker-compose, update environment variables and HAProxy configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Substitute environment variables in the template file
-envsubst '$DOMAIN,$RABBITMQ_DOMAIN,$PGADMIN_DOMAIN,$PORTAINER_DOMAIN,$CLIENT_APP_DOMAIN,$HAPROXY_STATS_PASS' < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
+envsubst '$DOMAIN,$RABBITMQ_DOMAIN,$PGADMIN_DOMAIN,$PORTAINER_DOMAIN,$CLIENT_APP_DOMAIN,$HAPROXY_STATS_PASS,$REDISINSIGHT_DOMAIN' < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 
 # Optional: Display the generated config for debugging
 echo "--- Generated haproxy.cfg ---"
